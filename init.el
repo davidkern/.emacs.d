72301a77465299f1efdb23b2962bbbe716f7ff07
David Kern <david@mju.io>
2020-02-22 20:13:15 -0800

    Configure rust-mode

--- init.el
+++ init.el
@@ -20,7 +20,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (projectile helm use-package))))
+ '(package-selected-packages
+   (quote
+    (flycheck-rust toml-mode lsp-ui lsp-mode company flycheck cargo rust-mode projectile helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,14 +35,14 @@
   :ensure t
   :config
   (load-theme 'doom-one t))
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (menu-bar-mode -1)
-;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
 (setq initial-frame-alist '((fullscreen . maximized)))
 
-;; Symbol-completion, auto-pair
+;; Helm
 (use-package helm
   :ensure t
   :bind (("M-x" . helm-M-x)
@@ -57,6 +59,8 @@
 	      ("M-H" . helm-end-of-buffer))
   :config
   (helm-mode 1))
+
+;; Auto-pair
 (electric-pair-mode)
 
 ;; Projectile
@@ -77,5 +81,42 @@
   (interactive)
   (find-file user-init-file))
 
-;; Keyboard mapping
 (global-set-key (kbd "C-c f i") #'dlk/find-user-init-file)
+
+;; Flycheck - syntax checking
+(use-package flycheck
+  :ensure t
+  :hook (prog-mode . flycheck-mode))
+
+;; Company - complete anything
+(use-package company
+  :ensure t
+  :hook (prog-mode . company-mode)
+  :config
+  (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1))
+
+;; Language Server Protocol
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :config (require 'lsp-clients))
+
+(use-package lsp-ui
+  :ensure t)
+
+;; Rust
+(use-package toml-mode
+  :ensure t)
+
+(use-package rust-mode
+  :ensure t
+  :hook (rust-mode . lsp))
+
+(use-package cargo
+  :ensure t
+  :hook (rust-mode . cargo-minor-mode))
+
+(use-package flycheck-rust
+  :ensure t
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
